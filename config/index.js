59e7621d44bee75b94f2a670475781e510a245e8
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-03-10 22:52:17 +0500

    updated /config/index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://webdev-newz-app.vercel.app";
+export const server = dev ? "http://localhost:3000" : "https:yourwebsite.com";
